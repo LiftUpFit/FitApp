f0446cf4a708a7ac6f7539fd66248a828ebacfd1
Richard Slagle <richard@liftupclothing.com>
2025-07-10 21:47:27 -0700

    Updated 7/10

---
+++ LiftUpFit/ExerciseLoggingView.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseLoggingView.swift
+//  LiftUpFit
+//
+//  Created by Richard Slagle on 6/29/25.
+//
+
+import SwiftUI
+
+struct ExerciseLoggingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseLoggingView()
+}
